a41a765438922e48a1ecc7bfbcda727f8062cf66
Pankaj Prakash <pprakash@cloudleaf.io>
2021-07-01 17:15:19 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+const fs = require("fs").promises;
+const getTheme = require("./theme");
+
+const lightTheme = getTheme({
+  style: "light",
+  name: "Guru Light",
+});
+
+// Write themes
+fs.mkdir("./themes", { recursive: true })
+  .then(() => Promise.all([fs.writeFile("./themes/light.json", JSON.stringify(lightTheme, null, 2))]))
+  .catch(() => process.exit(1));
